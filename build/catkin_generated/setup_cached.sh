b8a4888202c44a4413d8507817074d463ed59976
gtraines <gtraines@hotmail.com>
2018-09-16 12:05:30 -0700

    setting workspace root

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/robot1/robot1-ros/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/robot1/robot1-ros/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages:~/.local/lib/python2.7/site-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/robot1/robot1-ros/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/robot1/robot1-ros/src:$ROS_PACKAGE_PATH"

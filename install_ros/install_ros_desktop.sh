87ac3ce1d2adcad3a793288b77edd7ebcceceffa
gtraines <gtraines@hotmail.com>
2019-02-16 21:34:55 -0700

    What's the fuss?

--- install_ros/install_ros_desktop.sh
+++ install_ros/install_ros_desktop.sh
@@ -3,5 +3,5 @@ sudo apt-get install ros-kinetic-desktop-full -y
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-echo "export PYTHONPATH=/usr/lib/python2.7/dist-packages:$PATH" >> ~/.bashrc
+echo "export PYTHONPATH=/usr/lib/python2.7/dist-packages:$PYTHONPATH" >> ~/.bashrc
 source ~/.bashrc

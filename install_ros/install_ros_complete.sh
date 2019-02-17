3792a0f21f693ac5b3fabcab82bdf8ccc5069444
gtraines <gtraines@hotmail.com>
2019-02-16 23:02:35 -0700

    Fix directory finding

--- install_ros/install_ros_complete.sh
+++ install_ros/install_ros_complete.sh
@@ -1,5 +1,9 @@
 #! /usr/env/bin bash
 
-./add_repo_sources.sh
-./install_ros_desktop.sh
-./install_reqd_pkgs.sh
+# This line gets the name of the actual directory where the script is located,
+# not just the working directory
+SCRIPT_DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+bash ${SCRIPT_DIRNAME}/add_repo_sources.sh
+bash ${SCRIPT_DIRNAME}/install_ros_desktop.sh
+bash ${SCRIPT_DIRNAME}/install_reqd_pkgs.sh
